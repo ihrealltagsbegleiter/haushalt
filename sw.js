f9fab27f040e40f11873088aac0d2d3d13ecc337
ihrealltagsbegleiter <ihrealltagsbegleiter@gmail.com>
2026-06-04 01:58:19 +0200

    fix: cache v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "haushalt-v3";
+const CACHE = "haushalt-v4";
 const ASSETS = ["/haushalt/", "/haushalt/index.html", "/haushalt/manifest.json", "/haushalt/icon-192.png", "/haushalt/icon-512.png"];
 
 self.addEventListener("install", e => {
